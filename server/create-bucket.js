d696fdba726d7b445a277a6f7c3b8c6f8dab1842
nfinnegan <nfinneg2@gmail.com>
2021-09-24 16:06:41 -0500

    fixed connection error

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -3,7 +3,8 @@ const AWS = require('aws-sdk')
 const {v4: uuidv4} = require('uuid')
 
 //Set the region
-AWS.config.update({region:'us-east-2'});
+
+AWS.config.update({region: 'us-east-2'});
 
 //Create S3 service object//creates s3 instance object with designated API
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
