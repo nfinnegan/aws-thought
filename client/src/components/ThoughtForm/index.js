6257473f8a77fd206b80a76ef787105135638c68
nfinnegan <nfinneg2@gmail.com>
2021-11-03 14:05:42 -0500

    1 fix

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -20,6 +20,7 @@ const ThoughtForm = () => {
     event.preventDefault();
     const data = new FormData();
     data.append("image", fileInput.current.files[0]);
+
     const postImage = async () => {
       try {
         const res = await fetch("/api/image-upload", {
@@ -30,7 +31,7 @@ const ThoughtForm = () => {
         if (!res.ok) throw new Error(res.statusText);
         const postResponse = await res.json();
         setFormState({ ...formState, image: postResponse.Location });
-
+        console.log("postImage: ", postResponse.Location);
         return postResponse.Location;
       } catch (error) {
         console.log(error);
